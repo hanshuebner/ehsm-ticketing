2cb20cde16bcce30b224aafbb20fea942c45275c
Hans Huebner <hans.huebner@gmail.com>
2013-10-21 22:38:32 +0200

    payment executed

--- src/ehsm/core.clj
+++ src/ehsm/core.clj
@@ -7,20 +7,74 @@
             [ring.middleware.logger :as ring-logger]
             [ring.middleware.resource :as ring-resource]
             [ring.middleware.session :as ring-session]
-            [clj-paymill.net :refer :all]
-            [clj-paymill.client :refer :all]))
+            [clj-paymill.net :as paymill-net]
+            [clj-paymill.client :as paymill-client]))
 
 (defonce default-port 7676)
+(defonce paymill-private-key "f0a966a7f4d01204c4712def21a9f73d")
 
 (defn paymill-callback [req]
   (println "PAYMILL-CALLBACK" (:body req))
   {:status 200
    :body "ok"})
 
+(defn make-paymill-transaction [request]
+  (paymill-net/paymill-request paymill-private-key :post
+                               "transactions"
+                               {"amount" (:amount request)
+                                "currency" (or (:currency request) "EUR")
+                                "token" (:token request)
+                                "description" (:description request)}))
+
+(def error-strings {10001 "General undefined response."
+                    10002 "Still waiting on something."
+
+                    20000 "General success response."
+
+                    40000 "General problem with data."
+                    40001 "General problem with payment data."
+                    40100 "Problem with credit card data."
+                    40101 "Problem with cvv."
+                    40102 "Card expired or not yet valid."
+                    40103 "Limit exceeded."
+                    40104 "Card invalid."
+                    40105 "Expiry date not valid."
+                    40106 "Credit card brand required."
+                    40200 "Problem with bank account data."
+                    40201 "Bank account data combination mismatch."
+                    40202 "User authentication failed."
+                    40300 "Problem with 3d secure data."
+                    40301 "Currency / amount mismatch"
+                    40400 "Problem with input data."
+                    40401 "Amount too low or zero."
+                    40402 "Usage field too long."
+                    40403 "Currency not allowed."
+
+                    50000 "General problem with backend."
+                    50001 "Country blacklisted."
+                    50100 "Technical error with credit card."
+                    50101 "Error limit exceeded."
+                    50102 "Card declined by authorization system."
+                    50103 "Manipulation or stolen card."
+                    50104 "Card restricted."
+                    50105 "Invalid card configuration data."
+                    50200 "Technical error with bank account."
+                    50201 "Card blacklisted."
+                    50300 "Technical error with 3D secure."
+                    50400 "Decline because of risk issues."
+                    50500 "General timeout."
+                    50501 "Timeout on side of the acquirer."
+                    50502 "Risk management transaction timeout."
+                    50600 "Duplicate transaction."})
+
 (defn pay [req]
-  (println "PAY token" (:paymillToken (:params req)))
-  {:status 200
-   :body "ok"})
+  (let [{:keys [paymillToken amount currency description]} (:params req)
+        result (make-paymill-transaction {:token paymillToken :amount amount :currency currency :description description})]
+    (if (= (:response_code result) 20000)
+      {:status 200 :body "ok"}
+      (do
+        (println "payment failed" result)
+        {:status 423 :body "payment failed"}))))
 
 (defn not-found [req]
   {:status 404
