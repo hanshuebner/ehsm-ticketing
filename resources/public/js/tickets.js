df7b2ccd08f2f215a185e568298ef3ae39559297
Hans Huebner <hans.huebner@gmail.com>
2014-03-14 06:42:19 +0100

    remove config stuff that did not work

--- resources/public/js/tickets.js
+++ resources/public/js/tickets.js
@@ -11,19 +11,18 @@ angular
             .when('/registered', { templateUrl: 'partials/registered.html', })
             .otherwise({ templateUrl: '/partials/buy.html' });
     })
-    .directive('tickets', [ '$rootScope', function ($rootScope) {
+    .directive('tickets', [ function () {
         return {
             restrict: 'E',
             templateUrl: '/tickets.html'
         };
     }])
-    .controller('TicketsController', ['$scope', '$http', '$location', '$resource', '$rootScope',
-                                      function ($scope, $http, $location, $resource, $rootScope) {
+    .controller('TicketsController', ['$scope', '$http', '$location', '$resource',
+                                      function ($scope, $http, $location, $resource) {
         $location.path('/buy');
 
         $resource('client-config').get(function (data) {
             PAYMILL_PUBLIC_KEY = data.paymillPublicKey;
-            $rootScope.config = data;
         });
 
         $scope.ticket = localStorage.ticket ? JSON.parse(localStorage.ticket) : { donation: 0, type: 'supporter'};
